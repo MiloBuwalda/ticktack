3f84b1140a33f65c779f4d5590df26fb93262af2
Tim <t.d.haberham@students.uu.nl>
2015-09-04 02:52:00 +0200

    Basic menu screens with clickable buttons. (hacky positioning/scaling)

---
+++ TickTick/Assets/Scripts/Help.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// Help  (replacement for HelpState.cs)
+/// Assigned to Main camera in Help scene
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Help : MonoBehaviour {
+
+    // textures assigned in unity inspector
+    public Texture backgroundTexture;
+    public Texture backButtonTexture;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        // TODO remove these magic numbers for scaling (based on screen size & texture size of xna game)
+        float width = 0.16f * Screen.width;
+        float height = 0.065f * Screen.height;
+        if( GUI.Button(new Rect((Screen.width - width) / 2, (int)(Screen.height * 0.9), width, height), backButtonTexture, GUIStyle.none))
+        {
+            Application.LoadLevel("TitleMenu");
+        }
+
+    }
+}
--- TickTick/Assets/Scripts/titleMenu.cs
+++ TickTick/Assets/Scripts/titleMenu.cs
@@ -1,7 +1,34 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// TitleMenu  (replacement for TitlemenuState.cs)
+/// Assigned to Main camera in TitleMenu scene
+/// </summary>
+using UnityEngine;
 using System.Collections;
 
-public class titleMenu : MonoBehaviour
+public class TitleMenu : MonoBehaviour
 {
+
+    // textures assigned in unity inspector
+    public Texture backgroundTexture;
+    public Texture playButtonTexture;
+    public Texture helpButtonTexture;
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        // TODO remove these magic numbers for scaling (based on screen size & texture size of xna game)
+        float width = 0.16f * Screen.width;
+        float height = 0.065f * Screen.height;
+        if (GUI.Button(new Rect((Screen.width - width) / 2, (int)(Screen.height * 0.65), width, height), playButtonTexture, GUIStyle.none))
+        {
+            Application.LoadLevel("LevelMenu");
+        }
+        if (GUI.Button(new Rect((Screen.width - width) / 2, (int)(Screen.height * 0.72), width, height), helpButtonTexture, GUIStyle.none))
+        {
+            Application.LoadLevel("Help");
+        }
+    }
 
 }
